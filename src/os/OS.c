884ee35664a5719b2e88c096eae2bec8020575f9
David Day <david.day2017@gmail.com>
2023-01-07 19:22:08 -0600

    remove NVIC INT15 enable as it is not the systick interrupt

--- src/os/OS.c
+++ src/os/OS.c
@@ -9,7 +9,6 @@
 #include "OS.h"
 #include "DebugTools.h"
 
-#define NVIC_EN0_INT15 0x8000
 #define SYSTICK_RELOAD 79      // 1 us period for 80 MHz system clock
 
 static uint32_t SysTimeMicroSeconds;
@@ -18,7 +17,6 @@ void OS_InitSysTime(void) {
     NVIC_ST_CTRL_R &= 0x7;  // clear SysTick control register
     NVIC_ST_RELOAD_R &= 0x00FFFFFF;
     NVIC_ST_RELOAD_R = SYSTICK_RELOAD;
-    NVIC_EN0_R |= NVIC_EN0_INT15;  // enable SysTick interrupt in NVIC
     NVIC_ST_CTRL_R |= 7;  // enable clock with interrupts, clk src is sys clock
 }
 
