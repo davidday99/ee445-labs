643477dab96e19e0e87b88d7d0fd2118cef58594
David Day <david.day2017@gmail.com>
2023-01-07 18:01:50 -0600

    clean up main function

--- src/main.c
+++ src/main.c
@@ -11,33 +11,17 @@
 #include "interpreter.h"
 #include "OS.h"
 #include "sema.h"
+#include "DebugTools.h"
 
-#define ADC_SAMPLE_COUNT    1000
-#define ADC_SAMPLE_FREQ     2000
-
-uint16_t SampleBuf[ADC_SAMPLE_COUNT];
 char *Input;
 
-//debug code
 int main(void){
-    PLL_Init(Bus80MHz);       // set system clock to 80 MHz
+    PLL_Init(Bus80MHz);
     Interpreter_Init();
     ST7735_InitR(INITR_GREENTAB);
     OS_InitSysTime();
     ST7735_Message(0, 0, "Hello, world!", 0);
-
-    ADC_Open(ADC_CHANNEL11);
-    uint16_t s = ADC_In();
-
-    ST7735_Message(0, 1, "Sampled ", s);
-
-    ADC_Collect(ADC_CHANNEL11, ADC_SAMPLE_COUNT, SampleBuf, ADC_SAMPLE_COUNT);
-
-    while (ADC_Status() != 0)
-        ;
-
-    ST7735_Message(1, 0, "Sampling Done!", 0);
-
+    DebugTools_Init();
     ST7735_Message(1, 1, "System Time: ", OS_ReadPeriodicTime());
 
     while(1){
