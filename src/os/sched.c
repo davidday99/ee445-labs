f8f748faf7370a8f247fab284f903183aa725bfb
David Day <david.day2017@gmail.com>
2023-01-15 23:53:47 -0600

    make RunningThreadList circular doubly linked, fix Sched_Removethread

--- src/os/sched.c
+++ src/os/sched.c
@@ -1,45 +1,67 @@
 #include "sched.h"
 #include "thread.h"
 
-static Tcb_t *RunningThreadList;
-static Tcb_t *CurrentThread;
+Tcb_t *RunningThreadList;
+Tcb_t *CurrentThread;
 
 Tcb_t *Sched_GetCurrentThread(void) {
     return CurrentThread;
 }
 
 int Sched_AddThread(Tcb_t *t) {
+    t->state = RUNNING;
     if (RunningThreadList == 0) {
         RunningThreadList = t;
+        RunningThreadList->prev = RunningThreadList;
+        RunningThreadList->next = RunningThreadList;
     } else {
         Tcb_t *next = RunningThreadList;
-        while (next->next != 0) {
+
+        while (next->next != RunningThreadList && t->priority >= next->priority) {
             next = next->next;
         }
+
+        t->next = next->next;
+
+        /* If inserting at the end of the list, point head->prev to new element */
+        if (next->next == RunningThreadList) { 
+            RunningThreadList->prev = t;
+        }
+
         next->next = t;
         t->prev = next;
-        t->next = 0;
     }
     return 1;
 } 
 
 int Sched_RemoveThread(Tcb_t *t) {
     Tcb_t *next = RunningThreadList;
+
     while (next != t) {
         if (next == 0) {
             return 0;
         }
+        next = next->next;
     }
-    if (next->prev == 0 && next->next == 0) {
+
+    if (next->next == next) {
         RunningThreadList = 0;
+    } else {
+        t->prev->next = t->next;
+        t->next->prev = t->prev;
     }
-    if (next->prev == 0) {
-        RunningThreadList = next->next;
-        RunningThreadList->prev = 0;
+
+    /* If removing head, update head to next in list */
+    if (t == RunningThreadList) {
+        RunningThreadList = t->next;
     }
-    next->prev = next->next;
-    if (next->next != 0) {
-        next->next->prev = next->prev; 
+
+    /* If removing the current thread, update accordingly */
+    if (t == CurrentThread) {
+        CurrentThread = 0;
     }
+
+    t->state = IDLE;
     return 1;
 }
+
